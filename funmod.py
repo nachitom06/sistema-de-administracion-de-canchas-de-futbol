c38c0b65f8f0f9c1337b90982848dadf2e684c09
nachitom06 <i.agustin.monzon06@gmail.com>
2025-08-20 15:59:20 -0300

    Update funmod.py

--- funmod.py
+++ funmod.py
@@ -1,16 +1,16 @@
 def hola():
     print("hola")
 
-def cargar_precios_canchas():
-    print("los precios de las canchas son: futbol 5=$40000, futbol 8=$70000, futbol 11=$100000")
+def guardar_precio_cantidad_horas(num1,num2,num3):
+    print("los precios de las canchas son: futbol 5=$",num1,", futbol 8=$",num2,", futbol 11=$",num3)
     cobrar=int(input("ingrese numero de cancha(futbol 5, futbol 8, futbol 11)"))
     precio=0
     if cobrar==5:
-        precio=40000
+        precio=num1
     elif cobrar==8:
-        precio=70000
+        precio=num2
     else:
-        precio=100000
+        precio=num3
     canthoras=int(input("ingrese la cantidad de horas alquiladas de la cancha"))
     return precio,canthoras
 
@@ -18,3 +18,11 @@ def calcular_cantidad_a_pagar(a,b):
     """objetivo: indicar la cantidad que el cliente debe pagar"""
     plata=lambda a,b: a*b
     return plata(a,b)
+
+
+
+"""agregar lista o matriz, ademas de las dos que ya tenemos, donde acumulamos la recaudacion por canchas y por horarios
+    agregar funcion ListadoCanchas de: cantidad de formas de pago(tarjeta 10% mas), cancha de mayor recaudacion y menor 
+    recaudacion, incremento por incluir la tarjeta.
+    cambiar el precio de las canchas por un random.randint
+    def validacion de datos"""
