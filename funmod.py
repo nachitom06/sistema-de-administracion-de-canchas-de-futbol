67f69890419576776a80f1bc9976ae4c5fdf23d8
nachitom06 <i.agustin.monzon06@gmail.com>
2025-08-20 10:26:55 -0300

    Update funmod.py

--- funmod.py
+++ funmod.py
@@ -1,3 +1,8 @@
 def hola():
     print("hola")
-    
+
+
+def cantidad_a_pagar(canch):
+    """objetivo: indicar la cantidad que el cliente debe pagar"""
+    plata=lambda a,b,c: a*b
+    return plata
