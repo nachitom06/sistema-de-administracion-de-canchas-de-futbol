c1035e7ad045c57c6b75291d6df2f2549cb39c8d
nachitom06 <i.agustin.monzon06@gmail.com>
2025-08-20 10:33:37 -0300

    Update funmod.py

--- funmod.py
+++ funmod.py
@@ -4,7 +4,7 @@ def hola():
 def cargar_precios_canchas():
     print(los precios de las canchas son: futbol 5=$40000, futbol 8=$70000, futbol 11=$100000")
 
-def cantidad_a_pagar(a,b):
+def calcular_cantidad_a_pagar(a,b):
     """objetivo: indicar la cantidad que el cliente debe pagar"""
     plata=lambda a,b: a*b
     return plata
