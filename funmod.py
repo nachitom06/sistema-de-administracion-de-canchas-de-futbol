95e861f79f45c77ce6bafc5ac08eff8e11033ee1
nachitom06 <i.agustin.monzon06@gmail.com>
2025-08-20 13:38:22 -0300

    Update funmod.py

--- funmod.py
+++ funmod.py
@@ -17,4 +17,4 @@ def cargar_precios_canchas():
 def calcular_cantidad_a_pagar(a,b):
     """objetivo: indicar la cantidad que el cliente debe pagar"""
     plata=lambda a,b: a*b
-    return plata
+    return plata(a,b)
