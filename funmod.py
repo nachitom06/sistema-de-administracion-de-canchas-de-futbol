3c0eeb4bf334da018cb226c41c6678d333a6656a
nachitom06 <i.agustin.monzon06@gmail.com>
2025-08-20 10:39:40 -0300

    Update funmod.py

--- funmod.py
+++ funmod.py
@@ -3,6 +3,16 @@ def hola():
 
 def cargar_precios_canchas():
     print(los precios de las canchas son: futbol 5=$40000, futbol 8=$70000, futbol 11=$100000")
+    cobrar=int(input("ingrese numero de cancha(futbol 5, futbol 8, futbol 11)"))
+    precio=0
+    if cobrar==5:
+        precio=40000
+    elif cobrar==8:
+        precio=70000
+    else:
+        precio=100000
+    canthoras=int(input("ingrese la cantidad de horas alquiladas de la cancha"))
+    return precio,canthoras
 
 def calcular_cantidad_a_pagar(a,b):
     """objetivo: indicar la cantidad que el cliente debe pagar"""
