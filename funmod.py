adb95befd1fab69937407f0fcd1cba1cb68c27c9
nachitom06 <i.agustin.monzon06@gmail.com>
2025-08-27 14:14:08 -0300

    Update funmod.py

--- funmod.py
+++ funmod.py
@@ -9,6 +9,7 @@ def iniciar_matriz():
 def listado(recaudo,numerocanch):
     lista=list(zip(recaudo,numerocanch))
     lista.sort(key=lambda x:x[0])
+    lista.reverse()
     return lista
 
 def guardar_precio_cantidad_horas(num1,num2,num3,horarioscomp):
